01bc3304771dd241bc672a3c3b116a9b2fcd5448
JaretWright <Jaret.Wright@GeorgianCollege.ca>
2023-09-15 10:47:52 -0400

    Added listener's for the button's.

---
+++ app/src/main/java/com/example/f23comp3025w2/ExampleOfActivity.kt
@@ -0,0 +1,11 @@
+package com.example.f23comp3025w2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExampleOfActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example_of)
+    }
+}
--- app/src/main/java/com/example/f23comp3025w2/MainActivity.kt
+++ app/src/main/java/com/example/f23comp3025w2/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.f23comp3025w2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.example.f23comp3025w2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +21,20 @@ class MainActivity : AppCompatActivity() {
         //step 3 - connect the view to binding root
         setContentView(binding.root)
 
-        binding.button.setOnClickListener {
-            
+        binding.toastButton.setOnClickListener {
+            Toast.makeText(this, "This is a toast", Toast.LENGTH_LONG).show()
+        }
+
+        binding.snackbarButton.setOnClickListener {
+            Snackbar.make(binding.layout,"This is a snackbar message",Snackbar.LENGTH_LONG).show()
+        }
+
+        binding.snackbarWithActionButton.setOnClickListener {
+            val snackbar = Snackbar.make(binding.layout, "Should we exit class?", Snackbar.LENGTH_INDEFINITE)
+            snackbar.setAction("Click Here", View.OnClickListener {
+
+            })
+            snackbar.show()
         }
     }
 }
